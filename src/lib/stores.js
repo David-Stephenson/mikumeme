5844c08af3ce22ed385d30b4b4f9f2aaf49a7e71
David-Stephenson <52610115+David-Stephenson@users.noreply.github.com>
2023-12-26 14:28:25 -0500

    Update stores.js
    Add the ability to save stores to local storage

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,3 +1,21 @@
 import { writable } from 'svelte/store';
 
-export const videoHistory = writable([]);
+// Save stroes to local storage
+function localStore(key, initialValue) {
+	const initial =
+		typeof window !== 'undefined' && localStorage.getItem(key)
+			? JSON.parse(localStorage.getItem(key))
+			: initialValue;
+	const store = writable(initial);
+
+	store.subscribe((value) => {
+		if (typeof window !== 'undefined') {
+			localStorage.setItem(key, JSON.stringify(value));
+		}
+	});
+
+	return store;
+}
+
+export const pipedInstances = localStore('pipedInstances', []);
+export const videoHistory = localStore('videoHistory', []);
